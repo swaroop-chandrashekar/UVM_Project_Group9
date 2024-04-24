1cc95c4dc19be6f6eebef5f0e7b99dbd21992e45
Smit-Patel-22 <167924598+Smit-Patel-22@users.noreply.github.com>
2024-04-23 23:21:56 -0700

    Add files via upload
    Uploaded compiled the rptr_handler file, half empty condition remaining

---
+++ RTL Code/rptr_handler.sv
@@ -0,0 +1,51 @@
+module rptr_handler #(parameter ADDR_SIZE = 12)
+    (rEmpty,
+     raddr,
+     rptr,
+     wptr_s,
+     rinc, 
+     rclk, 
+     rrst
+    );
+
+output logic rEmpty;
+output logic [ADDR_SIZE-1:0] raddr;
+output logic [ADDR_SIZE :0] rptr;
+input logic [ADDR_SIZE :0] wptr_s;
+input logic rinc;
+input logic rclk;
+input logic rrst;
+
+logic [ADDR_SIZE:0] rbin;                                      //register for read pointer in binary
+logic rEmpty_reg;                                              //register whether fifo empty or not
+logic [ADDR_SIZE:0] rgraynext;                                 //next read pointer in gray code
+logic [ADDR_SIZE:0] rbinnext;                                  //next read pointer in binary form
+
+always_ff @(posedge rclk or negedge rrst)
+    begin
+        if (!rrst) begin
+            {rbin, rptr} <= 0;
+        end
+        else begin
+            {rbin, rptr} <= {rbinnext, rgraynext};             //when reset is low it will set to 0 otherwise point to next location
+        end
+    end
+
+assign raddr = rbin[ADDR_SIZE-1:0];                            //read address of the fifo
+
+always_comb begin                                              //read pointer logic
+    assign rbinnext = rbin + (rinc & ~rEmpty);                 //updated when rinc is high and fifo is not empty
+    assign rgraynext = (rbinnext>>1) ^ rbinnext;               //binary to gray code conversion 
+end
+
+assign rEmpty_reg = (rgraynext == wptr_s);                     //empty generation logic
+
+always_ff @(posedge rclk or negedge rrst)                      
+    begin
+        if (!rrst)
+        rEmpty <= 1'b1;
+        else 
+        rEmpty <= rEmpty_reg;
+    end
+
+endmodule
